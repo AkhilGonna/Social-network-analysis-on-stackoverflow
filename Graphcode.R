2f7c4152a5cfcb500da5d4aa15dc75b5f07799bf
AkhilGonna <56095525+AkhilGonna@users.noreply.github.com>
2019-11-04 23:33:27 -0700

    Update Graphcode.R

--- Graphcode.R
+++ Graphcode.R
@@ -9,7 +9,8 @@ link<-data.frame(
   asker=ask_ans[1],
   answerer=ask_ans[2])
 
-plot(net,vertex.size = 3,vertex.color = 'yellow',edge.width = .2,
+net1<-graph_from_data_frame(d=link_big, directed=T)
+plot(net1,vertex.size = 3,vertex.color = 'yellow',edge.width = .2,
      edge.color = 'black',edge.arrow.size=.02, vertex.label = NA)
 
 library(CINNA)
@@ -19,8 +20,8 @@ link_big<-data.frame(
   asker=gaint[2][[1]][,1],
   answer=gaint[2][[1]][,2]
 )
-net1<-graph_from_data_frame(d=link_big, directed=T)
-plot(net1,vertex.size = 4,vertex.color= 'yellow',edge.width = .6,
+net2<-graph_from_data_frame(d=link_big, directed=T)
+plot(net2,vertex.size = 4,vertex.color= 'yellow',edge.width = .6,
      edge.color = 'black',edge.arrow.size=.02, vertex.label = NA)
 
 write.table(link_big, file='gaint_component.tsv', quote=FALSE, sep='\t', col.names = NA)
