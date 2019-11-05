5f920135e2cc944ea60f703441619bd59b32698e
AkhilGonna <56095525+AkhilGonna@users.noreply.github.com>
2019-11-05 00:55:02 -0700

    Update Graphcode.R

--- Graphcode.R
+++ Graphcode.R
@@ -9,12 +9,12 @@ link<-data.frame(
   asker=ask_ans[1],
   answerer=ask_ans[2])
 
-net1<-graph_from_data_frame(d=link_big, directed=T)
+net1<-graph_from_data_frame(d=link, directed=T)
 plot(net1,vertex.size = 3,vertex.color = 'yellow',edge.width = .2,
      edge.color = 'black',edge.arrow.size=.02, vertex.label = NA)
 
 library(CINNA)
-gaint<-giant_component_extract(net,directed = TRUE)
+gaint<-giant_component_extract(net1,directed = TRUE)
 
 link_big<-data.frame(
   asker=gaint[2][[1]][,1],
